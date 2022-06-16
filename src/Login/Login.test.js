82fbedb0440b58ce734977c9e8364674a2c6ffdf
LeonAvancini <avancinileon@gmail.com>
2022-06-16 12:40:16 +0200

    added new styles and testing functions on Login Component

--- src/Login/Login.test.js
+++ src/Login/Login.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Login from ".";
 
 test("login text should be rendered", () => {
@@ -19,7 +19,7 @@ test("password input should be rendered", () => {
   expect(passwordInputEl).toBeInTheDocument();
 });
 
-test("login button should be rendered", () => {
+test("button should be rendered", () => {
   render(<Login />);
   const buttonEl = screen.getByRole("button");
   expect(buttonEl).toBeInTheDocument();
@@ -35,4 +35,47 @@ test("password input should be empty", () => {
   render(<Login />);
   const passwordInputEl = screen.getByPlaceholderText(/password/i);
   expect(passwordInputEl.value).toBe("");
+});
+
+test("button should be disabled", () => {
+  render(<Login />);
+  const buttonEl = screen.getByRole("button");
+  expect(buttonEl).toBeDisabled();
+});
+
+test("error message should not be visible", () => {
+  render(<Login />);
+  const errorEl = screen.getByTestId("error-text");
+  expect(errorEl).not.toBeVisible();
+});
+
+test("username input should change", () => {
+  render(<Login />);
+  const usernameInputEl = screen.getByPlaceholderText(/username/i);
+  const testValue = "test";
+  fireEvent.change(usernameInputEl, { target: { value: testValue } });
+  expect(usernameInputEl.value).toBe(testValue);
+});
+
+test("password input should change", () => {
+  render(<Login />);
+  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+  const testValue = "test";
+  fireEvent.change(passwordInputEl, { target: { value: testValue } });
+  expect(passwordInputEl.value).toBe(testValue);
+});
+
+test("button should not be disabled if input has value", () => {
+  render(<Login />);
+  const buttonEl = screen.getByRole("button");
+
+  const usernameInputEl = screen.getByPlaceholderText(/username/i);
+  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+
+  const testValue = "test";
+
+  fireEvent.change(usernameInputEl, { target: { value: testValue } });
+  fireEvent.change(passwordInputEl, { target: { value: testValue } });
+
+  expect(buttonEl).not.toBeDisabled();
 });
--- src/Login/index.js
+++ src/Login/index.js
@@ -24,27 +24,82 @@ const FormContainer = styled.div`
   align-items: center;
 `;
 
+const InputStyled = styled.input`
+  outline: 1px solid #fafafa;
+  padding: 0.5rem;
+  margin: 1rem 0rem 0.5rem 0rem;
+  font-size: 1.2rem;
+  border-radius: 0.5rem;
+  transition: 0.3s ease-in;
+
+  &:focus {
+    transition: 0.3s ease-in;
+    outline: 1px solid #000000;
+  }
+`;
+
+const ButtonStyled = styled.button`
+  outline: 1px solid #fafafa;
+  background: #fafafa;
+  padding: 0.5rem 2rem;
+  margin: 1rem 0rem 0.5rem 0rem;
+  font-size: 1.2rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  transition: 0.3s ease-in;
+
+  &:hover,
+  :focus {
+    transition: 0.3s ease-in;
+    background: #d3d3d3;
+    outline: 1px solid #000000;
+  }
+
+  &:active {
+    transition: 0s ease-in;
+    outline: 1px solid #000000;
+    background: #a9a9a9;
+  }
+
+  &:disabled {
+    outline: 1px solid #fafafa;
+    background: #fafafa;
+    cursor: auto;
+  }
+`;
+
+const ErrorMessage = styled.span`
+  color: red;
+  visibility: ${(props) => (props.isVisible ? "visible" : "hidden")};
+`;
+
 export const Login = () => {
   const [userNameValue, setUserNameValue] = useState("");
   const [passwordValue, setpasswordValue] = useState("");
+  const [isError, setIsError] = useState(false);
 
   return (
     <Container>
       <H1Styled data-testid="login-text">Login</H1Styled>
       <FormContainer>
-        <input
+        <InputStyled
           type="text"
           placeholder="Username"
           value={userNameValue}
           onChange={(event) => setUserNameValue(event.target.value)}
         />
-        <input
+        <InputStyled
           type="password"
           placeholder="Password"
           value={passwordValue}
           onChange={(event) => setpasswordValue(event.target.value)}
         />
-        <button disabled={!userNameValue || !passwordValue}>Login</button>
+        <ButtonStyled disabled={!userNameValue || !passwordValue}>
+          Login
+        </ButtonStyled>
+        <ErrorMessage isVisible={isError} data-testid="error-text">
+          Something went wrong!
+        </ErrorMessage>
       </FormContainer>
     </Container>
   );
